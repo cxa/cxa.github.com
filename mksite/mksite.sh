955835bbb4f8c6d10131b9a6f84c426a469b2e18
CHEN Xian-an <xianan.chen@gmail.com>
2020-12-11 00:05:13 +0800

    Update html minify script

--- mksite/mksite.sh
+++ mksite/mksite.sh
@@ -4,6 +4,6 @@ make
 ./mksite
 make clean
 
-postcss ../assets/style.css --no-map -u autoprefixer postcss-preset-env cssnano -o ../assets/style.min.css
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --input-dir ../posts --output-dir ../posts_ && rm -rf ../posts && mv ../posts_ ../posts
-html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --input-dir ../til --output-dir ../til_ && rm -rf ../til && mv ../til_ ../til
+cd ..
+postcss ./assets/style.css --no-map -u autoprefixer postcss-preset-env cssnano -o ./assets/style.min.css
+find . ./posts ./til -name "*.html" -depth 1 -exec html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace {} -o {}.tmp \; -exec mv {}.tmp {} \;
