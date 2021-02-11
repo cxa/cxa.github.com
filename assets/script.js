5933926112493e228ea44430e218c72d6f50a66a
CHEN Xian-an <xianan.chen@gmail.com>
2021-02-11 17:04:22 +0800

    Add utterances

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,18 @@
 window.WebFontConfig = {
   custom: {
-    families: ["Anonymous Pro Minus:n4,n7,i4,i7", "Atkinson Hyperlegible:n4,n7,i4,i7"],
+    families: [
+      "Anonymous Pro Minus:n4,n7,i4,i7",
+      "Atkinson Hyperlegible:n4,n7,i4,i7",
+    ],
     urls: ["/assets/fonts.css"],
   },
   timeout: 60000,
-  active: function() {
-    document.documentElement.className = document.documentElement.className.replace(/no\-webfont/, "");
-  }
+  active: function () {
+    document.documentElement.className = document.documentElement.className.replace(
+      /no\-webfont/,
+      ""
+    );
+  },
 };
 
 (function (d) {
@@ -18,5 +24,16 @@ window.WebFontConfig = {
   wf.src = "/assets/webfontloader.js";
   wf.async = true;
   s.parentNode.insertBefore(wf, s);
-  
+
+  var post = d.getElementById("post");
+  if (post) {
+    var ut = d.createElement("script");
+    ut.setAttribute("src", "https://utteranc.es/client.js");
+    ut.setAttribute("repo", "cxa/cxa.github.com");
+    ut.setAttribute("issue-term", "pathname");
+    ut.setAttribute("theme", "preferred-color-scheme");
+    ut.setAttribute("crossorigin", "anonymous");
+    ut.setAttribute("async", "async");
+    post.getElementsByTagName("main")[0].appendChild(ut);
+  }
 })(document);
